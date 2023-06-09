431a937539c8e020983cc50e841f5f3398e372e0
Денис <Allimp_94@mail.ru>
2023-06-09 19:38:52 +0300

    тест фронт

--- source/data/main.js
+++ source/data/main.js
@@ -7,6 +7,6 @@ export default ({ error, pageName, status }) => ({
 		}
 	},
 	description: 'Лендинг для telegram фриланс биржи.',
-	pixelperfect: JSON.stringify({ breakpoints: [320, 660, 1200], ext: 'webp' }),
+	pixelperfect: JSON.stringify({ breakpoints: [375, 768, 1300], ext: 'webp' }),
 	projectName: 'TelegaADS'
 });
